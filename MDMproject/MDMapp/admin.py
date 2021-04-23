634d42a1cd72597faa3a2c4d62f9b4522417da72
Rand West City <randwestcityGit@gmail.com>
2021-04-23 15:18:04 +0200

    added User and Cell Model

--- MDMproject/MDMapp/admin.py
+++ MDMproject/MDMapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserCapture, CellphoneCapture
 
-# Register your models here.
+admin.site.register(UserCapture)
+admin.site.register(CellphoneCapture)
