c751e442c74ccbb86552cda76db0927ef9787378
Rand West City <randwestcityGit@gmail.com>
2021-04-23 15:19:33 +0200

    updated urls

--- MDMproject/MDMapp/urls.py
+++ MDMproject/MDMapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name="index"),
+    path('enroll/', views.enroll, name="enroll"),
+    path('userview/', views.user_info, name="userview"),
+    path('cellview/', views.cellphone_info, name="cellview"),
 
 ]
